140165eb0fc6741f23abd0c31b278e5f1eabcc8b
Lakshya-111 <108977200+Lakshya-111@users.noreply.github.com>
2022-10-16 09:33:20 +0530

    Update waterManagement.dart

--- lib/pages/waterManagement.dart
+++ lib/pages/waterManagement.dart
@@ -53,7 +53,24 @@ class _WaterManagementState extends State<WaterManagement> {
                         children: <Widget>[
                           Padding(
                             padding: EdgeInsets.fromLTRB(25, 0, 25, 20),
-                            child: Text("",
+                            child: Text("""Diseases: In humans, drinking or consuming polluted water in any way has many disastrous effects on our health. It causes typhoid, cholera, hepatitis and various other diseases.
+
+Eradication of Ecosystem: Ecosystem is extremely dynamic and responds to even small changes in the environment. Increasing water pollution can cause an entire ecosystem to collapse if left unchecked.
+
+Eutrophication: Chemicals accumulation and infusion in a water body, encourages the growth of algae. The algae form a layer on top of the pond or lake. Bacteria feed on this algae and this event decreases the amount of oxygen in the water body, severely affecting the aquatic life there
+
+Effects of the food chain: Turmoil in food chain happens when the aquatic animals (fish, prawns, seahorse, etc) consume the toxins and pollutants in the water,  and then the humans consume them.
+
+Prevention of Water Pollution
+The best way to prevent large-scale water pollution is to try and reduce its harmful effects. There are numerous small changes we can make to protect ourselves from a future where water is scarce.
+
+Conserve Water: Conserving water should be our first aim. Water wastage is a major problem globally and we are only now waking up to the issue. Simple small changes made domestically will make a huge difference.
+
+Treatment of sewage: Treating waste products before disposing of it in water bodies helps reduce water pollution on a large scale. Agriculture or other industries can reuse this wastewater by reducing its toxic contents.
+
+Use of environment-friendly products: By using soluble products that do not go on to become pollutants, we can reduce the amount of water pollution caused by a household.
+
+""",
 
                               textAlign: TextAlign.center,
                               style : TextStyle(
